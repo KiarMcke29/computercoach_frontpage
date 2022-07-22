05dde0246a484991c3bbf1da4411bc1f3c49836b
KiarMcke29 <kiaraimckenzie@gmail.com>
2022-07-22 14:32:03 -0400

    Made nav overlay more responsive
    Still didn't fix overlay drop down

--- front_page_redesign/main.js
+++ front_page_redesign/main.js
@@ -1,9 +1,23 @@
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+  document.getElementById("myNav").style.width = "100%";
+}
 
- 
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+var dropdown = document.getElementsByClassName("dropnav");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+  } else {
+    dropdownContent.style.display = "block";
+  }
+});
+console.log("Hello");
+}
